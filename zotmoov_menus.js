61a044c5249447084119bb89c05968c79ca7f12d
Wiley Yu <wileyyu@stanford.edu>
2023-06-26 16:26:39 -0700

    Fix many things

--- zotmoov_menus.js
+++ zotmoov_menus.js
@@ -8,35 +8,33 @@ ZotMoov_Menus = {
     _getWindow()
     {
         var enumerator = Services.wm.getEnumerator("navigator:browser");
-        var window = null;
         while (enumerator.hasMoreElements()) {
             let win = enumerator.getNext();
             if (!win.ZoteroPane) continue;
-            window = win;
+            return win;
         }
-        return window;
     },
 
     init()
     {
-        let win = this._getWindow()
+        let win = this._getWindow();
         let doc = win.document;
-        // createElementNS() necessary in Zotero 6; createElement() defaults to HTML in Zotero 7
-        let HTML_NS = "http://www.w3.org/1999/xhtml";
-        let XUL_NS = "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul";
 
         // Menu separator
-        let menuseparator = doc.createElementNS(XUL_NS, 'menuseparator');
+        let menuseparator = doc.createXULElement('menuseparator');
 
         // Menu item
-        let move_selected_item = doc.createElementNS(XUL_NS, 'menuitem');
+        let move_selected_item = doc.createXULElement('menuitem');
         move_selected_item.id = 'zotmoov-context-move-selected';
         move_selected_item.setAttribute('data-l10n-id', 'zotmoov-context-move-selected');
-        move_selected_item.onclick = Zotero.ZotMoov.moveSelectedItems();
+        move_selected_item.addEventListener('command', () => {
+            Zotero.ZotMoov.moveSelectedItems();
+        });
 
         let zotero_itemmenu = doc.getElementById('zotero-itemmenu');
         zotero_itemmenu.appendChild(menuseparator);
         zotero_itemmenu.appendChild(move_selected_item);
+
         this._store_added_elements.push(menuseparator, move_selected_item);
 
         // Enable localization
@@ -45,8 +43,7 @@ ZotMoov_Menus = {
 
     destroy()
     {
-        let win = this._getWindow()
-        let doc = win.document;
+        let doc = this._getWindow().document;
         for (let element of this._store_added_elements)
         {
             if (element) element.remove();
