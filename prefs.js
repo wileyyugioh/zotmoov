ebbd801767b786792d36ca5747cf9e5a7feefbdd
Wiley Yu <wileyyu@stanford.edu>
2023-06-25 23:28:26 -0700

    Init

---
+++ prefs.js
@@ -0,0 +1,7 @@
+// ZotMoov
+// prefs.js
+// Written by Wiley Yu
+
+pref('extensions.zotmoov.dst_dir', '');
+pref('extensions.zotmoov.enable_custom_filename', true);
+pref('extensions.zotmoov.delete_file_after_copy', false);
