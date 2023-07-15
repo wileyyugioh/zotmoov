823b65b784aa884ad218dc6edf336cf5d60871cb
Wiley Yu <wileyyu@stanford.edu>
2023-07-15 03:20:56 -0700

    Reformat bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -2,17 +2,20 @@
 // bootstrap.js
 // Written by Wiley Yus
 
-if (typeof Zotero == 'undefined') {
+if (typeof Zotero == 'undefined')
+{
     var Zotero;
 }
 
 var ZotMoov_Menus;
 
-function log(msg) {
+function log(msg)
+{
     Zotero.debug('ZotMoov: ' + msg);
 }
 
-async function install() {    
+async function install()
+{    
     log('ZotMoov: Installed');
 
     // Fix for old version parity
@@ -24,11 +27,13 @@ async function install() {
     }
 }
 
-async function startup({ id, version, resourceURI, rootURI = resourceURI.spec }) {
+async function startup({ id, version, resourceURI, rootURI = resourceURI.spec })
+{
     Services.scriptloader.loadSubScript(rootURI + 'zotmoov.js');
     Services.scriptloader.loadSubScript(rootURI + 'zotmoov_menus.js');
 
-    Zotero.PreferencePanes.register({
+    Zotero.PreferencePanes.register(
+    {
         pluginID: 'zotmoov@wileyy.com',
         src: rootURI + 'prefs.xhtml',
         scripts: [rootURI + 'zotmoov_prefs.js']
@@ -38,7 +43,8 @@ async function startup({ id, version, resourceURI, rootURI = resourceURI.spec })
     ZotMoov_Menus.init();
 }
 
-function shutdown() {
+function shutdown()
+{
     log('ZotMoov: Shutting down');
     Zotero.ZotMoov.destroy();
     ZotMoov_Menus.destroy();
@@ -47,6 +53,7 @@ function shutdown() {
     ZotMoov_Menus = null;
 }
 
-function uninstall() {    
+function uninstall()
+{    
     log('ZotMoov: Uninstalled');
 }
