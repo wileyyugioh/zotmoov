2a3eacccc28944465b2a4fffee635e692bf5b9c7
Wiley Yu <none@none.com>
2024-03-15 17:34:27 -0700

    Fix bug with classes getting reloaded

--- bootstrap.js
+++ bootstrap.js
@@ -3,8 +3,9 @@
 // Written by Wiley Yu
 
 // Declare zotmoov and zotmoovMenus at the top level
-var zotmoov = null;
-var zotmoovMenus = null;
+let first_startup = true;
+let zotmoov = null;
+let zotmoovMenus = null;
 
 function log(msg)
 {
@@ -26,14 +27,19 @@ async function install()
 
 async function startup({ id, version, resourceURI, rootURI = resourceURI.spec })
 {
-    // Only ones we need to load directly here
-    Services.scriptloader.loadSubScript(rootURI + 'init/00-script-definitions.js');
-    Services.scriptloader.loadSubScript(rootURI + 'init/01-script-loader.js');
+    if (first_startup)
+    {
+        // Only ones we need to load directly here
+        Services.scriptloader.loadSubScript(rootURI + 'init/00-script-definitions.js');
+        Services.scriptloader.loadSubScript(rootURI + 'init/01-script-loader.js');
 
-    let scriptPaths = new ScriptDefinitions().getScriptPaths();
-    let scriptLoader = new ScriptLoader(rootURI);
+        let scriptPaths = new ScriptDefinitions().getScriptPaths();
+        let scriptLoader = new ScriptLoader(rootURI);
 
-    await scriptLoader.loadScripts(scriptPaths);
+        await scriptLoader.loadScripts(scriptPaths);
+
+        first_startup = false;
+    }
 
     const directoryManager = new DirectoryManager();
     const outputManager = new OutputManager(directoryManager);
@@ -42,8 +48,8 @@ async function startup({ id, version, resourceURI, rootURI = resourceURI.spec })
     const sanitizer = new Sanitizer();
     const zotmoovWildcard = new ZotMoovWildcard(sanitizer);
 
-    let zotmoov = new ZotMoov(id, version, zotmoovWildcard, sanitizer, zotmoovDebugger);
-    let zotmoovMenus = new ZotMoovMenus(zotmoov);
+    zotmoov = new ZotMoov(id, version, zotmoovWildcard, sanitizer, zotmoovDebugger);
+    zotmoovMenus = new ZotMoovMenus(zotmoov);
 
     Zotero.PreferencePanes.register(
         {
@@ -71,6 +77,10 @@ function shutdown()
     log('ZotMoov: Shutting down');
     zotmoov.destroy();
     zotmoovMenus.unloadAll();
+
+    zotmoov = null;
+    zotmoovMenus = null;
+    Zotero.ZotMoov = null;
 }
 
 function uninstall()
