896e5be2d63c86a7c8e98d9b2597b2d4398de700
Wiley Yu <wileyyu@stanford.edu>
2024-04-04 23:04:07 -0700

    Move patching to new file

---
+++ src/05-zotmoov-patcher.js
@@ -0,0 +1,32 @@
+var ZotMoovPatcher = class {
+    constructor()
+    {
+        this._disabled = false;
+        this._orig_funcs = [];
+    }
+
+    monkey_patch(object, method, patcher)
+    {
+        let orig_func = object[method];
+        let new_func = patcher(orig_func);
+
+        let self = this;
+        object[method] = function(...args)
+        {
+            if (self._disabled) return orig_func.apply(this, args);
+            return new_func.apply(this, args)
+        };
+
+        return this._orig_funcs.push(orig_func) - 1;
+    }
+
+    get_orig_func(id)
+    {
+        return this._orig_funcs[id];
+    }
+
+    destroy()
+    {
+        this._disabled = true;
+    }
+}
