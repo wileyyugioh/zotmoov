6477a6b6e408fb7425c2bed17a3cfbe51840637b
Wiley Yu <wileyyu@stanford.edu>
2024-09-25 22:51:49 -0700

    Make copy group library pref default to true

--- prefs.js
+++ prefs.js
@@ -9,7 +9,7 @@ pref('extensions.zotmoov.enable_subdir_move', false);
 pref('extensions.zotmoov.file_behavior', 'move');
 pref('extensions.zotmoov.subdirectory_string', '{%c}');
 pref('extensions.zotmoov.auto_process_delay', 5000);
-pref('extensions.zotmoov.copy_group_libraries', false)
+pref('extensions.zotmoov.copy_group_libraries', true)
 pref('extensions.zotmoov.allowed_fileext', JSON.stringify(['pdf', 'epub', 'docx']))
 pref('extensions.zotmoov.delete_files', false);
 pref('extensions.zotmoov.prune_empty_dir', true);
