b210cb38bd1e31f0cf9c75ae115ad63dba326187
Wiley Yu <wileyyu@stanford.edu>
2024-10-14 23:52:59 -0700

    Work on warning for conflicting keybind

--- preferences/zotmoov-keyboard-prefs.js
+++ preferences/zotmoov-keyboard-prefs.js
@@ -1,6 +1,25 @@
 class ZotMoovKeyboardPrefs {
+
+    static get BOUND_KEYS()
+    {
+        return [
+            Zotero.Prefs.get('extensions.zotero.keys.saveToZotero', true),
+            Zotero.Prefs.get('extensions.zotero.keys.newItem', true),
+            Zotero.Prefs.get('extensions.zotero.keys.newNote', true),
+            Zotero.Prefs.get('extensions.zotero.keys.library', true),
+            Zotero.Prefs.get('extensions.zotero.keys.quicksearch', true),
+            Zotero.Prefs.get('extensions.zotero.keys.copySelectedItemCitationsToClipboard', true),
+            Zotero.Prefs.get('extensions.zotero.keys.copySelectedItemsToClipboard', true),
+            Zotero.Prefs.get('extensions.zotero.keys.sync', true),
+            Zotero.Prefs.get('extensions.zotero.keys.toggleAllRead', true),
+            Zotero.Prefs.get('extensions.zotero.keys.toggleRead', true)
+        ];
+    }
+
     init()
     {
+        this._zotero_bound_keys = this.constructor.BOUND_KEYS;
+
         for (let label of document.querySelectorAll('#zotmoov-kb-settings-sc-grid .modifier'))
         {
             // Display the appropriate modifier keys for the platform
@@ -15,14 +34,35 @@ class ZotMoovKeyboardPrefs {
                 let pref = textbox.getAttribute('preference');
                 const val = Zotero.Prefs.get(pref, true).toUpperCase();
                 textbox.value = val || '';
+                
                 Zotero.ZotMoov.Menus.rebindPrefToKey(textbox.getAttribute('preference'), textbox.value);
+                if (this._zotero_bound_keys.includes(textbox.value))
+                {
+                    this.constructor.addWarningTooltip(textbox.parentElement.parentElement);
+                } else
+                {
+
+                }
             });
             textbox.addEventListener('input', () =>
             {
                 textbox.value = textbox.value.toUpperCase();
                 Zotero.ZotMoov.Menus.rebindPrefToKey(textbox.getAttribute('preference'), textbox.value);
+                if (this._zotero_bound_keys.includes(textbox.value))
+                {
+                    this.constructor.addWarningTooltip(textbox.parentElement.parentElement);
+                } else
+                {
+
+                }
             });
         }
+    }
+
+    static addWarningTooltip(element)
+    {
+        element.style.color = 'red';
+        element.title = 'Warning!!!!';
     }
 }
 
