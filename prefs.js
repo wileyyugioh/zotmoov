74db4045e0a0856e2666e9a7190cd43b831e5ad4
Wiley Yu <wileyyu@stanford.edu>
2024-12-01 14:31:56 -0800

    Add default file ext djvu

--- prefs.js
+++ prefs.js
@@ -10,7 +10,7 @@ pref('extensions.zotmoov.file_behavior', 'move');
 pref('extensions.zotmoov.subdirectory_string', '{%c}');
 pref('extensions.zotmoov.auto_process_delay', 5000);
 pref('extensions.zotmoov.copy_group_libraries', true)
-pref('extensions.zotmoov.allowed_fileext', JSON.stringify(['pdf', 'epub', 'docx']))
+pref('extensions.zotmoov.allowed_fileext', JSON.stringify(['pdf', 'epub', 'docx', 'djvu']))
 pref('extensions.zotmoov.delete_files', false);
 pref('extensions.zotmoov.prune_empty_dir', true);
 pref('extensions.zotmoov.enable_attach_dir', false);
